59fb819bda278946142155b585852671de162e19
Sushindh <sushindh.anandan@gmail.com>
2024-07-14 13:21:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Signin from "./SignInPage.jsx"
+
+
+function App() {
+  return(
+    <>
+    <Signin/>
+    </>
+  );
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens:{
+        'max-w-600px':{'max':'600px'},
+        'min-w-600px':{'min':'600px'},
+      },
+      fontFamily: {
+        'fonts': ['"Post No Bills Colombo ExtraBold"','sans-serif'],
+        'fam':['"Post No Bills Jaffna ExtraBold"','sans-serif'],
+      }
+    },
+  },
+  
+}
